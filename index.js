642523d481ddc91e545f917205b871342e32ec9d
KJ59 <klarasedi@gmail.com>
2025-02-23 12:48:34 +0100

    homework

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+let chosenNumber = Number(prompt("Please write down a number from 0 - 36"));
+const chosenNumberIsEven = chosenNumber%2 == 0;
+const inFirstRange = (((chosenNumber > 0 && chosenNumber <= 10) || (chosenNumber > 18 &&  chosenNumber <= 28)))
+const inSecondRange = ((((chosenNumber > 10 && chosenNumber <= 19) || (chosenNumber > 28 && chosenNumber <= 36))))
+
+if (chosenNumber === 0) { 
+    document.body.innerHTML += `Your number 0 is green`;
+} else if ((chosenNumberIsEven && inFirstRange) || (!chosenNumberIsEven && inSecondRange)) { 
+document.body.innerHTML += `Your number ${chosenNumber} is <b>black.</b>`} 
+else {document.body.innerHTML += `Your number ${chosenNumber} is <b>red.</b>` ;}
