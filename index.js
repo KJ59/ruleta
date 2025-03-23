ab1f8927bb9b1fb8ff5dd2170862a3429d72e4ed
KJ59 <klarasedi@gmail.com>
2025-03-23 19:51:12 +0100

    uprava

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-
-
 let chosenNumber = Number(prompt("Please write down a number from 0 - 36"));
 const chosenNumberIsEven = chosenNumber%2 == 0;
 const inFirstRange = (((chosenNumber > 0 && chosenNumber <= 10) || (chosenNumber > 18 &&  chosenNumber <= 28)))
 const inSecondRange = ((((chosenNumber > 10 && chosenNumber <= 19) || (chosenNumber > 28 && chosenNumber <= 36))))
 
-if (chosenNumber === 0) { 
+if (chosenNumber < 0 | chosenNumber > 36 | Number.isInteger(chosenNumber) === false) {
+    document.body.innerHTML +=`Your number is invalid. Type down a number 0 - 36`}
+else if (chosenNumber === 0) { 
     document.body.innerHTML += `Your number 0 is green`;
 } else if ((chosenNumberIsEven && inFirstRange) || (!chosenNumberIsEven && inSecondRange)) { 
 document.body.innerHTML += `Your number ${chosenNumber} is <b>black.</b>`} 
